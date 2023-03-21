471bc56902d8ae4790848cf626284e3e20cbff72
DenisLuba <delubis@mail.ru>
2023-03-22 00:47:41 +0300

    Finished the task 'area_or_perimeter'

---
+++ src/main/kotlin/area_or_perimeter/Solution.kt
@@ -0,0 +1,5 @@
+package area_or_perimeter
+
+fun areaOrPerimeter(l: Int, w: Int) =
+    if (l == w) l * w
+    else 2 * ( l + w)
---
+++ src/main/kotlin/area_or_perimeter/TestExample.kt
@@ -0,0 +1,12 @@
+package area_or_perimeter
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TestExample {
+    @Test
+    fun testAreaOrPerimeter() {
+        assertEquals(32, areaOrPerimeter(6, 10))
+        assertEquals(9, areaOrPerimeter(3, 3))
+    }
+}
