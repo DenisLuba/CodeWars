f4e63352286067398b23bbdc390809efb2c2d8cb
DenisLuba <delubis@mail.ru>
2023-03-22 00:56:25 +0300

    modifed area_or_perimeter

--- src/main/kotlin/area_or_perimeter/TestExample.kt
+++ src/main/kotlin/area_or_perimeter/TestExample.kt
@@ -8,5 +8,6 @@ class TestExample {
     fun testAreaOrPerimeter() {
         assertEquals(32, areaOrPerimeter(6, 10))
         assertEquals(9, areaOrPerimeter(3, 3))
+        assertEquals(16, areaOrPerimeter(4, 4))
     }
 }
